31c7d8047a2cf8872470acddf45002c4e52b9d1c
drboben <drbobenr@gmail.com>
2025-06-21 20:53:36 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './'
-});
+  base: '/epub-reader/', // el nombre exacto de tu repositorio
+  build: {
+    outDir: 'dist'
+  }
+})
